f5fcba87eecf31876fd770151ed0f08a112e21e5
gbbgd3 <williambrar6@gmail.com>
2023-11-29 06:21:44 -0600

    search working

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -6,7 +6,17 @@ class ProductController < ApplicationController
   def search
     @category = Category.find_by(id: params[:category])
     @search = params[:search]
-    @sort_order = params[:sort_order]
-
+    @sort_order = params[:sort]
+    @products = Product.all
+    case @sort_order
+      when 1 # sale
+        @products = Product.where("price_cents > sale_price_cents")
+      when 2 # new
+        @products = Product.where("created_at >= ?", 3.days.ago).order(created_at: :desc)
+      end
+    @products = @products.where(category: @category) if @category.present?
+    @products = @products.where("product_name LIKE ?", "%#{@search}%")
+    @products_total = @products.count
+    @products = @products.where("product_name LIKE ?", "%#{@search}%").page(params[:page]).per(30)
   end
 end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,10 +2,10 @@ class SearchController  < ApplicationController
   def search_redirect
     id = params[:category].to_i
     search = params[:search].to_s
+    sort_order = params[:sort].present? ? params[:sort].to_i : 0
     c= Category.find_by(id: id)
-
     if c
-      redirect_to send("#{(CGI.escape(c.name).downcase).gsub('+', '_')}_search_path", search: search, category: c)
+      redirect_to send("#{(CGI.escape(c.name).downcase).gsub('+', '_')}_search_path", search: search, category: c, sort: sort_order)
       return
     end
     redirect_to products_search_path(search: search)
