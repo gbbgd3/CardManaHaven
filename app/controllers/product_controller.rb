5f941502d10a91155990f8616ab65a4852b73478
gbbgd3 <williambrar6@gmail.com>
2023-11-29 07:15:42 -0600

    Search completed, Navigation Completed

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,19 +1,20 @@
 class ProductController < ApplicationController
   def index
     @products = Product.page(params[:page]).per(30)
+    @products_total = Product.all.count
   end
 
   def search
     @category = Category.find_by(id: params[:category])
     @search = params[:search]
-    @sort_order = params[:sort]
+    @sort_order = params[:sort].to_i 
     @products = Product.all
     case @sort_order
-      when 1 # sale
-        @products = Product.where("price_cents > sale_price_cents")
-      when 2 # new
-        @products = Product.where("created_at >= ?", 3.days.ago).order(created_at: :desc)
-      end
+    when 1 # sale
+      @products = @products.where("price_cents > sale_price_cents")
+    when 2 # new
+      @products = @products.where("created_at >= ?", 3.days.ago).order(created_at: :desc)
+    end
     @products = @products.where(category: @category) if @category.present?
     @products = @products.where("product_name LIKE ?", "%#{@search}%")
     @products_total = @products.count
